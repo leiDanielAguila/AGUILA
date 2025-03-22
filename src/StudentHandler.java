f5e06dc3c3a72761f6bfed503215e778a429fedb
Lei <aguila.christleidaniel@gmail.com>
2025-03-22 16:58:14 +0800

    corrected version of student id handler
    In the initial code there was a database connection issue regarding the query was handled, to remove this corrected the query handler by using a prepared statement query

--- src/StudentHandler.java
+++ src/StudentHandler.java
@@ -13,21 +13,29 @@ public class StudentHandler {
 	public int studentIdHandler() {
 		boolean done = false;
 		int studentId = 0;
-		String query = "select count(*) from students where student_id = ?"; // check for duplicates
+
 		
 		while(!done) {
 	        System.out.print("Enter student id: ");
 	        
 	        try {
+	        	int input = in.nextInt();
+	        	
+	        	if (input <= 0 || input >= 1000) {
+	                System.out.println("student id can not be less than 1 and more than 1000.\n");
+	                continue;
+	            }
+	        	
 	        	Class.forName("com.mysql.cj.jdbc.Driver");
 	        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AGUILA", "root", "1234");
 	        	Statement st = connection.createStatement();
-	        	ResultSet rs = st.executeQuery(query);
-	            int input = in.nextInt();
+	        	
+	    		String query = "select count(*) from students where student_id = ?"; // check for duplicates
+	    		PreparedStatement ps = connection.prepareStatement(query);
+	    		ps.setInt(1, input);
+	        	ResultSet rs = ps.executeQuery();	      
 	            
-	            if (input <= 0 || input >= 1000) {
-	                System.out.println("student id can not be less than 1 and more than 1000.\n");
-	            } else if (rs.next() && rs.getInt(1) > 0) {
+	        	if (rs.next() && rs.getInt(1) > 0) {
 	            	System.out.println("student id exist in the database select another.\n");
 	            } else {
 	                done = true;

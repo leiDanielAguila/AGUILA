caab8c0ab7cda9ea874e8893a0dc3893e440cd9d
Lei <aguila.christleidaniel@gmail.com>
2025-03-22 22:17:38 +0800

    student account creation
    implemented the student account creation properly however the password creation is not secure and needs improvement

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
     	StudentHandler s = new StudentHandler();
     	
-    	System.out.println(s.firstNameHandler());
+    	s.makeNewStudent();
     }
 }
--- src/StudentHandler.java
+++ src/StudentHandler.java
@@ -11,6 +11,24 @@ public class StudentHandler {
 		
 		while (!done) {
 			System.out.println("--------Creating an account--------");
+			try {				
+				Class.forName("com.mysql.cj.jdbc.Driver");
+	        	Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/AGUILA", "root", "1234");
+	        	Statement st = connection.createStatement();
+				String query = "insert into students (student_id, first_name, last_name, password) values (?,?,?,?)";
+				PreparedStatement ps = connection.prepareStatement(query);
+				ps.setInt(1, studentIdHandler());
+				ps.setString(2, firstNameHandler());
+				ps.setString(3, lastNameHandler());
+				ps.setString(4, passwordHandler());
+				ps.executeUpdate();
+				System.out.println("----Account created please login----");
+				done = true;
+			} catch (ClassNotFoundException e) {
+	            System.out.println("JDBC Driver not found.-------");
+	        } catch (SQLException e) {
+	            System.out.println("Database connection error.-------");
+	        }
 		}
 	}
 	
@@ -24,9 +42,10 @@ public class StudentHandler {
 	        
 	        try {
 	        	int input = in.nextInt();
+	        	in.nextLine();
 	        	
 	        	if (input <= 0) {
-	                System.out.println("student id can not be less than 1. \n");
+	                System.out.println("-------student id can not be less than 1. \n");
 	                continue;
 	            }
 	        	
@@ -40,18 +59,18 @@ public class StudentHandler {
 	        	ResultSet rs = ps.executeQuery();	      
 	            
 	        	if (rs.next() && rs.getInt(1) > 0) {
-	            	System.out.println("student id exist in the database select another.\n");
+	            	System.out.println("------student id exist in the database select another.\n");
 	            } else {
 	                done = true;
 	                studentId = input;
 	            }
 	        } catch (InputMismatchException e) {
-	            System.out.println("Please enter a valid integer.\n");
+	            System.out.println("-------Please enter a valid integer.\n");
 	            in.next(); 
 	        } catch (ClassNotFoundException e) {
-	            System.out.println("JDBC Driver not found.");
+	            System.out.println("JDBC Driver not found.-------");
 	        } catch (SQLException e) {
-	            System.out.println("Database connection error.");
+	            System.out.println("Database connection error.-------");
 	        }
 	    }
 	    return studentId;
@@ -65,14 +84,14 @@ public class StudentHandler {
 			try {
 				String input = in.nextLine();
 				if (input.trim().isEmpty()) {
-					System.out.println("first name can not be empty.");
+					System.out.println("-------first name can not be empty.\n");
 					done = false;
 				} else {
 					firstName = input.toLowerCase();
 					done = true;
 				}
 			} catch (InputMismatchException e) {
-				System.out.println("Please enter a valid first name.\n");
+				System.out.println("-------Please enter a valid first name.\n");
 				in.next();
 			}
 		}
@@ -87,14 +106,14 @@ public class StudentHandler {
 			try {
 				String input = in.nextLine();
 				if (input.trim().isEmpty()) {
-					System.out.println("last name can not be empty.");
+					System.out.println("-------last name can not be empty.\\n");
 					done = false;
 				} else {
 					lastName = input.toLowerCase();
 					done = true;
 				}
 			} catch (InputMismatchException e) {
-				System.out.println("Please enter a valid last name.\n");
+				System.out.println("-------Please enter a valid last name.\n");
 				in.next();
 			}
 		}
@@ -109,14 +128,14 @@ public class StudentHandler {
 			try {
 				String input = in.nextLine();
 				if (input.trim().isEmpty()) {
-					System.out.println("password can not be empty.");
+					System.out.println("-------password can not be empty.\n");
 					done = false;
 				} else {
 					password = input.toLowerCase();
 					done = true;
 				}
 			} catch (InputMismatchException e) {
-				System.out.println("Please enter a valid password.\n");
+				System.out.println("-------Please enter a valid password.\n");
 				in.next();
 			}
 		}
